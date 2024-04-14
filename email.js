88b38ff13178369fe412b5803ec02b1fa98286f3
seysela <156629272+seysela@users.noreply.github.com>
2024-04-14 16:36:29 -0400

    email.js

--- email.js
+++ email.js
@@ -5,21 +5,16 @@ const port = 5500;
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get("/", (req, res) => {
-
-res.sendFile(__dirname + "/contact.html"); // send HTML file on GET request
-
+    res.sendFile(__dirname + "/contact.html"); // send HTML file on GET request
 });
-app.get('/', (req, res) => {
-    res.redirect('/contact.html'); // Redirect to contact.html or any other page
-  });
-app.post('/submit-form', (req, res) => {
 
-const fullname = req.body.fullname; // access form data
-const email = req.body.email;
-const phone = req.body.phone;
-const message = req.body.message;
-// Add validation logic here
-res.send('Your form has been sumbitted correctly.');
+app.post('/submit-form', (req, res) => {
+    const fullname = req.body.fullname; // access form data
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const message = req.body.message;
+    // Add validation logic here
+    res.send('Your form has been submitted correctly.');
 });
 
 app.listen(port, () => {
